ca5525d9375cb4d829429283411320eab4b88433
Nur Hanna <nurhanna01@users.noreply.github.com>
2026-01-28 10:11:11 +0700

    feat(search) : add custom analyzer (stopwords and lowercase)

--- src/elasticsearch/elasticsearch.bootstrap.ts
+++ src/elasticsearch/elasticsearch.bootstrap.ts
@@ -11,11 +11,28 @@ export async function bootstrapProductIndex() {
       mappings: {
         properties: {
           id: { type: 'keyword' },
-          name: { type: 'text' },
-          description: { type: 'text' },
+          name: { type: 'text', analyzer: 'my_analyzer' },
+          description: { type: 'text', analyzer: 'my_analyzer' },
           status: { type: 'keyword' },
           price: { type: 'integer' },
           createdAt: { type: 'date' },
+        },
+      },
+      settings: {
+        analysis: {
+          analyzer: {
+            my_analyzer: {
+              type: 'custom',
+              tokenizer: 'standard',
+              filter: ['indo_stop', 'lowercase'],
+            },
+          },
+          filter: {
+            indo_stop: {
+              type: 'stop',
+              stopwords: '_indonesian_',
+            },
+          },
         },
       },
     });
